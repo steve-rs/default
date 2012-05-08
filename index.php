7315945bf965d8558f3071f2d325626e571520c1
Stephen Bylo <steve@rightscale.com>
2012-05-08 18:47:22 +0800

    added '<?php', changed table name

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?
+<?php
 
 include 'config/db.php';
 
 mysql_connect($hostname_DB,$username_DB,$password_DB);
 @mysql_select_db($database_DB) or die( "Unable to select database");
-$query="SELECT * FROM phptest";
+$query="SELECT * FROM app_test";
 $result=mysql_query($query);
 
 $num=mysql_numrows($result);
@@ -16,8 +16,8 @@ echo "<b><center> Database($database_DB) Output From Host($hostname_DB)</center>
 $i=0;
 while ($i < $num) {
 
-$name=mysql_result($result,$i,"firstname");
-$value=mysql_result($result,$i,"lastname");
+$name=mysql_result($result,$i,"name");
+$value=mysql_result($result,$i,"value");
 
 
 echo "<b>$name:$value</b><br><hr><br>";
