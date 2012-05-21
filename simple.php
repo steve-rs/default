6993aa5709199986703f4509ecad7be864a29c67
Stephen Bylo <steve@rightscale.com>
2012-05-21 17:55:16 +0800

    ..

---
+++ simple.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "<b><center>";
+
+$file = "/var/spool/cloud/meta-data/instance-id";
+if (file_exists($file)) {
+    readfile($file);
+}
+else
+{
+	echo "Unknown Instance";
+}
+
+echo "</center></b>";
+
+?>
+
