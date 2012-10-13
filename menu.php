122d7c1af13730968a7fd9155b90504dad1992e3
Stephen Bylo <steve@rightscale.com>
2012-10-13 11:00:42 +0800

    ..

--- menu.php
+++ menu.php
@@ -1,9 +1,10 @@
 <p>
-<a href="health.php">health.php</a> - 
-<a href="index.php">index.php</a> - 
-<a href="load_test.php">load_test.php</a> - 
-<a href="mctest.php">mctest.php</a> - 
-<a href="orig.php">orig.php</a> - 
-<a href="simple.php">simple.php</a> - 
+| <a href="health.php">health.php</a> 
+| <a href="index.php">index.php</a> 
+| <a href="load_test.php">load_test.php</a> 
+| <a href="mctest.php">mctest.php</a> 
+| <a href="menu.php">menu.php</a> 
+| <a href="orig.php">orig.php</a> 
+| <a href="simple.php">simple.php</a> 
+|
 </p>
-
