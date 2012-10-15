01542769434a79373d3daa84d8049afc8ac4e9f1
Stephen Bylo <steve@rightscale.com>
2012-10-15 16:21:42 +0800

    ..

--- simple.php
+++ simple.php
@@ -1,15 +1,29 @@
 <?php
+
 include 'menu.php';
 
 echo "<b><center>";
 
 $file = "/tmp/server.metadata";
+
 if (file_exists($file)) {
-    readfile($file);
+    #readfile($file);
+    $lines = file($file);
+    // Loop through our array, show HTML source as HTML source; and line numbers too.
+    if ( $lines )
+    {
+        foreach ($lines as $line) {
+            echo htmlspecialchars($line) . "<br />\n";
+        }
+    }
+    else
+    {
+        echo "Meta data file is emply!";
+    }
 }
 else
 {
-	echo "No meta data available!";
+	echo "Meta data file missing or unreadable!";
 }
 
 echo "</center></b>";
