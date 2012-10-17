a28f0b3a95b41bee18fe42cdb9ce804990e26d10
Stephen Bylo <steve@rightscale.com>
2012-10-17 20:11:08 +0800

    ..

--- load_test.php
+++ load_test.php
@@ -1,12 +1,13 @@
 <?php
 
 include 'config/db.php';
-    #ob_clean();
-    #flush();
 
 include 'meta.php';
 
-isset ( $database_DB ) or die( "No database name set");
+isset ( $database_DB ) or die( "Database name not set");
+
+#string gethostbyname ( string $hostname )
+echo "Master DB IP Address (" . gethostbyname ( $hostname_DB ) . ")<br/>\n";
 
 mysql_connect($hostname_DB,$username_DB,$password_DB);
 #@mysql_select_db($database_DB) or die( "Unable to select database '$database_DB'");
@@ -18,29 +19,27 @@ $num=mysql_numrows($result);
 
 mysql_close();
 
-echo "<b><center> Database($database_DB) Output From Host($hostname_DB)</center></b><br><br>";
+echo "<b><center> Database ($database_DB) output from host ($hostname_DB)</center></b><br><br>";
 
-$file = "/var/spool/cloud/meta-data/instance-id";
-if (file_exists($file)) {
-    readfile($file);
-}
+#$file = "/var/spool/cloud/meta-data/instance-id";
+#if (file_exists($file)) {
+#    readfile($file);
+#}
 
-#echo 'HELLO';
 echo '<p><center>';
 
 $i=0;
 while ($i < $num) {
-
-$name=mysql_result($result,$i,"name");
-$value=mysql_result($result,$i,"value");
-
-echo "<b>$name:$value</b><br><hr><br>";
-
-$i++;
+   $name=mysql_result($result,$i,"name");
+   $value=mysql_result($result,$i,"value");
+   echo "<b>$name:$value</b><br><hr><br>";
+   $i++;
 }
-echo '</center>';
 
 echo "<b> Starting PHPINFO: </b><hr>";
+echo '</center>';
+
 phpinfo();
+
 ?>
 
