2275e44a4b4a1675e8437018cd5c5b3c04ce28c3
Stephen Bylo <steve@rightscale.com>
2012-10-18 12:14:18 +0800

    ..

--- menu.php
+++ menu.php
@@ -1,5 +1,4 @@
 <p>
-| <a href="geo.php">geo</a> 
 | <a href="health.php">health</a> 
 | <a href="index.php">index</a> 
 | <a href="load_test.php">load_test</a> 
@@ -7,6 +6,7 @@
 | <a href="menu.php">menu</a> 
 | <a href="meta.php">meta</a> 
 | <a href="orig.php">orig</a> 
+| <a href="tt.php">tt</a> 
 | <a href="haproxy-status">haproxy-status</a> 
 |
 </p>
