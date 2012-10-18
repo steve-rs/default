33a4312faa446fac2ae31c843b7240d26b85532c
Stephen Bylo <steve@rightscale.com>
2012-10-18 12:51:37 +0800

    ..

--- menu.php
+++ menu.php
@@ -2,21 +2,25 @@
 
 echo "<p>";
 
-if ($handle = opendir('.')) {
-    while (false !== ($entry = readdir($handle))) {
+#if ($handle = opendir('.')) {
+$files = listdir('.'); 
+sort($files, SORT_LOCALE_STRING); 
+foreach ($files as $entry) { 
+#    while (false !== ($entry = readdir($handle))) {
 	if ( preg_match ( '/^(.*)\.(php|html)$/', $entry, $match ) )
 	{
         	echo "| <a href=\"$entry\">$match[1]</a>\n";
 	}
-    }
+}
 
     echo "| <a href=\"haproxy-status\">haproxy-status</a> ";
     echo "|";
 
-    closedir($handle);
-}
+    #closedir($handle);
+#}
 
 echo "</p>";
 
 ?>
+
 
