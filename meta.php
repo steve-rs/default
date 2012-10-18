d9c737cc3298b013e9c511ae420b8bee30b00a18
Stephen Bylo <steve@rightscale.com>
2012-10-18 10:08:23 +0800

    ..

--- meta.php
+++ meta.php
@@ -34,11 +34,18 @@ else
 if ( isset ($pub_ip) ) {
 	require_once('geo/geoplugin.class.php');
 	$geoplugin = new geoPlugin();
-	$geoplugin->locate("23.20.26.42");
+	$geoplugin->locate($pub_ip);
 ?>
 	<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;aq=3&amp;sspn=40.188298,79.101563&amp;ie=UTF8&amp;hq=&amp;t=m&amp;z=7&amp;ll=<?php $geoplugin->longitude ?>,<?php $geoplugin->latitude ?>&amp;output=embed"></iframe>
 <?php
 }
+
+require_once('geo/geoplugin.class.php');
+$geoplugin = new geoPlugin();
+$geoplugin->locate("23.20.26.42");
+?>
+<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;aq=3&amp;sspn=40.188298,79.101563&amp;ie=UTF8&amp;hq=&amp;t=m&amp;z=7&amp;ll=<?php $geoplugin->longitude ?>,<?php $geoplugin->latitude ?>&amp;output=embed"></iframe>
+<?php
 
 echo "</center></b>";
 
