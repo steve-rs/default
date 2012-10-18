5b03d3a9fb422d9d7e7f0aa81db68426f46c2409
Stephen Bylo <steve@rightscale.com>
2012-10-18 11:47:09 +0800

    ..

--- meta.php
+++ meta.php
@@ -6,6 +6,10 @@ echo "<b><center>";
 
 $file = "/tmp/server.metadata";
 
+echo "<table>";
+echo "<tr>";
+echo "<td>";
+
 if (file_exists($file)) {
 	#readfile($file);
 	$lines = file($file);
@@ -15,14 +19,22 @@ if (file_exists($file)) {
 		foreach ($lines as $line)
 		{
 			echo htmlspecialchars($line) . "<br />\n";
+
+			# Look for public IP
 			if ( ! isset ( $pub_ip ) )
 			{
 				if ( preg_match ( '/^\d+\.\d+\.\d+\.\d+/', $line, $match ) )
 				{
-					if ( ! preg_match ( '/^10\.\d+\.\d+\.\d+/', $line ) )
+					if (
+						! preg_match ( '/^10\.\d+\.\d+\.\d+/', $line ) and 
+						! preg_match ( '/^192\.168\.\d+\.\d+/', $line ) and 
+						! preg_match ( '/^172\.1[6789]\.\d+\.\d+/', $line ) and
+						! preg_match ( '/^172\.2[0-9]\.\d+\.\d+/', $line ) and
+						! preg_match ( '/^172\.3[01]\.\d+\.\d+/', $line )
+					)
 					{
 						$pub_ip = $match[0];
-						echo "Matched [$pub_ip]<br>";
+						#echo "Matched [$pub_ip]<br>";
 					}
 				}
 			}
@@ -38,20 +50,28 @@ else
 	echo "Meta data file missing or unreadable!";
 }
 
+echo "</td>";
+
 if ( isset ($pub_ip) ) {
+	echo "<td>";
+
 	echo "pub ip = [$pub_ip] <br>";
 	require_once('geo/geoplugin.class.php');
 	$geoplugin = new geoPlugin();
 	$geoplugin->locate("$pub_ip");
 	$long = $geoplugin->longitude;
 	$lati = $geoplugin->latitude;
-	echo "long = $long <br>";
-	echo "lati = $lati <br>";
+	#echo "long = $long <br>";
+	#echo "lati = $lati <br>";
 ?>
 	<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
 src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;aq=3&amp;ie=UTF8&amp;hq=&amp;t=m&amp;z=5&amp;ll=<?php echo $lati ?>,<?php echo $long ?>&amp;output=embed"></iframe>
 <?php
+	echo "</td>";
 }
+
+echo "</tr>";
+echo "</table>";
 
 echo "</center></b>";
 
