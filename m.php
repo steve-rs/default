7dac4fb41f5055410c9a3976ff3180078df857d1
Stephen Bylo <steve@rightscale.com>
2012-10-18 12:21:56 +0800

    ..

---
+++ m.php
@@ -0,0 +1,16 @@
+<?php
+
+if ($handle = opendir('.')) {
+    echo "Directory handle: $handle\n";
+    echo "Entries:\n";
+
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($entry = readdir($handle))) {
+        echo "$entry\n";
+    }
+
+    closedir($handle);
+}
+
+?>
+
