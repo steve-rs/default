0fb60f8a8fe8ac990714d402c27db2ac9092caec
Stephen Bylo <steve@rightscale.com>
2012-10-18 10:54:05 +0800

    ..

--- meta.php
+++ meta.php
@@ -39,12 +39,12 @@ else
 }
 
 if ( isset ($pub_ip) ) {
-	echo "pub ip = $pub_ip <br>";
+	echo "pub ip = [$pub_ip] <br>";
 	require_once('geo/geoplugin.class.php');
 	$geoplugin = new geoPlugin();
 	$geoplugin->locate($pub_ip);
 ?>
-	<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;aq=3&amp;sspn=40.188298,79.101563&amp;ie=UTF8&amp;hq=&amp;t=m&amp;z=7&amp;ll=<?php $geoplugin->longitude ?>,<?php $geoplugin->latitude ?>&amp;output=embed"></iframe>
+	<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;aq=3&amp;ie=UTF8&amp;hq=&amp;t=m&amp;z=7&amp;ll=<?php $geoplugin->longitude ?>,<?php $geoplugin->latitude ?>&amp;output=embed"></iframe>
 <?php
 }
 
