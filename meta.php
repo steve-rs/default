69d3548a82c59698a09cdb2c76ff8d04ca28791f
Stephen Bylo <steve@rightscale.com>
2012-10-18 11:02:48 +0800

    ..

--- meta.php
+++ meta.php
@@ -43,6 +43,8 @@ if ( isset ($pub_ip) ) {
 	require_once('geo/geoplugin.class.php');
 	$geoplugin = new geoPlugin();
 	$geoplugin->locate("$pub_ip");
+	echo "long = $geoplugin->longitude <br>";
+	echo "lati = $geoplugin->latitude <br>";
 ?>
 	<iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;aq=3&amp;ie=UTF8&amp;hq=&amp;t=m&amp;z=7&amp;ll=<?php $geoplugin->longitude ?>,<?php $geoplugin->latitude ?>&amp;output=embed"></iframe>
 <?php
